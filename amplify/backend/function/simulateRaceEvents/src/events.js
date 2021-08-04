c45a3778b987cb0c306e559285ecd52b47df7447
codeolam <frankuchenna30@gmail.com>
2021-08-04 09:22:53 +0100

    added simulation .js files

---
+++ amplify/backend/function/simulateRaceEvents/src/events.js
@@ -0,0 +1,72 @@
+module.exports.events = [
+ {
+     type: "_lap",
+     competitor: "16",
+     lap: 1,
+     time: "1:26.573",
+     position: 1
+ },
+ {
+     type: "_lap",
+     competitor: "16",
+     lap: 2,
+     time: "1:25.194",
+     position: 1
+ },
+ {
+     type: "_lap",
+     competitor: "16",
+     lap: 3,
+     time: "1:24.954",
+     position: 1
+ },
+ {
+     type: "_lap",
+     competitor: "16",
+     lap: 4,
+     time: "1:24.949",
+     position: 1
+ },
+ {
+     type: "_lap",
+     competitor: "16",
+     lap: 5,
+     time: "1:24.907",
+     position: 1
+ },
+ {
+     type: "_lap",
+     competitor: "16",
+     lap: 6,
+     time: "1:24.900",
+     position: 1
+ },
+ {
+     type: "_lap",
+     competitor: "16",
+     lap: 7,
+     time: "1:25.062",
+     position: 1
+ },
+ {
+     type: "_lap",
+     competitor: "16",
+     lap: 8,
+     time: "1:25.024",
+     position: 1
+ },
+ {
+     type: "_lap",
+     competitor: "16",
+     lap: 9,
+     time: "1:24.987",
+     position: 1
+ },
+ {
+     type: "_lap",
+     competitor: "16",
+     lap: 10,
+     time: "1:25.010",
+     position: 1
+ }
+]
---
+++ amplify/backend/function/simulateRaceEvents/src/telemetry.js
@@ -0,0 +1,573 @@
+module.exports.events = [
+    {
+        longitude: 9.281044006347654,
+        latitude: 45.61760953240892,
+        speed: 331,
+        gear: 8
+    },
+    {
+        longitude: 9.281086921691893,
+        latitude: 45.61829991612166,
+        speed: 333,
+        gear: 8
+    },
+    {
+        longitude: 9.281301498413086,
+        latitude: 45.618990291334214,
+        speed: 336,
+        gear: 8
+    },
+    {
+        longitude: 9.281344413757324,
+        latitude: 45.61965064227938,
+        speed: 337,
+        gear: 8
+    },
+    {
+        longitude: 9.281387329101562,
+        latitude: 45.62034100086123,
+        speed: 339,
+        gear: 8
+    },
+    {
+        longitude: 9.281387329101562,
+        latitude: 45.621061365971045,
+        speed: 341,
+        gear: 8
+    },
+    {
+        longitude: 9.281516075134277,
+        latitude: 45.621691677849846,
+        speed: 342,
+        gear: 8
+    },
+    {
+        longitude: 9.281473159790039,
+        latitude: 45.62244203989484,
+        speed: 343,
+        gear: 8
+    },
+    {
+        longitude: 9.281644821166992,
+        latitude: 45.6232524196235,
+        speed: 347,
+        gear: 8
+    },
+    {
+        longitude: 9.28168773651123,
+        latitude: 45.623942733856815,
+        speed: 349,
+        gear: 8
+    },
+    {
+        longitude: 9.281773567199707,
+        latitude: 45.624182841162884,
+        speed: 351,
+        gear: 8
+    },
+    {
+        longitude: 9.281773567199707,
+        latitude: 45.624362920967556,
+        speed: 300,
+        gear: 7
+    },
+    {
+        longitude: 9.281859397888184,
+        latitude: 45.62451298702959,
+        speed: 220,
+        gear: 5
+    },
+    {
+        longitude: 9.281859397888184,
+        latitude: 45.62454300019377,
+        speed: 110,
+        gear: 3
+    },
+    {
+        longitude: 9.282116889953613,
+        latitude: 45.62454300019377,
+        speed: 100,
+        gear: 2
+    },
+    {
+        longitude: 9.282374382019043,
+        latitude: 45.62454300019377,
+        speed: 167,
+        gear: 4
+    },
+    {
+        longitude: 9.28271770477295,
+        latitude: 45.62454300019377,
+        speed: 192,
+        gear: 5
+    },  
+        {
+        longitude: 9.282631874084473,
+        latitude: 45.62469306577376,
+        speed: 197,
+        gear: 5
+},
+        {
+        longitude: 9.282546043395996,
+        latitude: 45.624933169866175,
+        speed: 202,
+        gear: 5
+},
+        {
+        longitude: 9.282288551330566,
+        latitude: 45.62520328574093,
+        speed: 213,
+        gear: 5
+},
+        {
+        longitude: 9.282159805297852,
+        latitude: 45.62550341296415,
+        speed: 222,
+        gear: 5
+},
+        {
+        longitude: 9.282159805297852,
+        latitude: 45.625833551053816,
+        speed: 232,
+        gear: 5
+},
+        {
+        longitude: 9.282073974609375,
+        latitude: 45.62607365026148,
+        speed: 242,
+        gear: 5
+},
+        {
+        longitude: 9.282073974609375,
+        latitude: 45.62688397749506,
+        speed: 256,
+        gear: 6
+},
+        {
+        longitude: 9.282417297363281,
+        latitude: 45.62757424701036,
+        speed: 269,
+        gear: 7
+},
+        {
+        longitude: 9.28246021270752,
+        latitude: 45.627994406901486,
+        speed: 286,
+        gear: 7
+},
+        {
+        longitude: 9.283146858215332,
+        latitude: 45.62865465179594,
+        speed: 298,
+        gear: 8
+},
+        {
+        longitude: 9.283747673034668,
+        latitude: 45.629134825016266,
+        speed: 310,
+        gear: 8
+},
+        {
+        longitude: 9.284305572509766,
+        latitude: 45.62940492064501,
+        speed: 317,
+        gear: 8
+},
+        {
+        longitude: 9.285120964050293,
+        latitude: 45.629645004555705,
+        speed: 319,
+        gear: 8
+},
+        {
+longitude: 9.286236763000488,
+        latitude: 45.62973503575707,
+        speed: 323,
+        gear: 8
+},
+        {
+        longitude: 9.28795337677002,
+        latitude: 45.630065148924906,
+        speed: 325,
+        gear: 8
+},
+        {
+        longitude: 9.289498329162598,
+        latitude: 45.630065148924906,
+        speed: 327,
+        gear: 8
+},
+        {
+longitude: 9.29069995880127,
+        latitude: 45.630275219928514,
+        gear: 6,
+        speed: 280
+},
+        {
+longitude: 9.291386604309082,
+        latitude: 45.630155179451435,
+        gear: 4,
+        speeD: 220
+},
+        {
+longitude: 9.291472434997559,
+        latitude: 45.630365250117606,
+        speed: 110,
+        gear: 3
+},
+        {
+        longitude: 9.29142951965332,
+        latitude: 45.630485290144776,
+        speed: 170,
+        gear: 4
+},
+        {
+        longitude: 9.29168701171875,
+        latitude: 45.63057531999642,
+        speed: 187,
+        gear: 4
+},
+        {
+        longitude: 9.29194450378418,
+        latitude: 45.630665349703456,
+        speed: 199,
+        gear: 5
+},
+        {
+        longitude: 9.292845726013184,
+        latitude: 45.63084540868369,
+        speed: 207,
+        gear: 5
+},
+        {
+        longitude: 9.293832778930664,
+        latitude: 45.63102546708549,
+        speed: 220,
+        gear: 5
+},
+        {
+longitude: 9.295120239257812,
+        latitude: 45.631205524908815,
+        speed: 225,
+        gear: 5
+},
+        {
+longitude: 9.295763969421387,
+        latitude: 45.63135557265304,
+        speed: 270,
+        gear: 6
+},
+        {
+        longitude: 9.296021461486816,
+        latitude: 45.63126554405472,
+        speed: 267,
+        gear: 6
+},
+        {
+        longitude: 9.296493530273438,
+        latitude: 45.63102546708549,
+        speed: 227,
+        gear: 6
+},
+        {
+longitude: 9.296493530273438,
+        latitude: 45.63081539889382,
+        speed: 210,
+        gear: 6
+},
+        {
+        longitude: 9.296665191650389,
+        latitude: 45.63057531999642,
+        speed: 210,
+        gear: 5
+},
+        {
+        longitude: 9.29662227630615,
+        latitude: 45.629915097726034,
+        speed: 250,
+        gear: 7
+},
+        {
+        longitude: 9.296708106994629,
+        latitude: 45.629494952232015,
+        speed: 240,
+        gear: 6
+},
+        {
+        longitude: 9.29662227630615,
+        latitude: 45.629134825016266,
+        speed: 210,
+        gear: 5
+},
+        {
+longitude: 9.296836853027344,
+        latitude: 45.62886472808596,
+        speed: 187,
+        gear: 4
+},
+        {
+        longitude: 9.296793937683105,
+        latitude: 45.62850459682101,
+        speed: 213,
+        gear: 5
+},
+        {
+        longitude: 9.29662227630615,
+        latitude: 45.62832453032085,
+        speed: 227,
+        gear: 5
+},
+        {
+        longitude: 9.296321868896484,
+        latitude: 45.628204485666025,
+        speed: 253,
+        gear: 6
+},
+        {
+        longitude: 9.295721054077148,
+        latitude: 45.62787436153968,
+        speed: 261,
+        gear: 7
+},
+        {
+longitude: 9.294133186340332,
+        latitude: 45.62736416588381,
+        speed: 270,
+        gear: 7
+},
+        {
+        longitude: 9.293231964111328,
+        latitude: 45.6269440012686,
+        speed: 291,
+        gear: 7
+},
+        {
+        longitude: 9.2924165725708,
+        latitude: 45.62655384559168,
+        speed: 316,
+        gear: 8
+},
+        {
+        longitude: 9.2909574508667,
+        latitude: 45.625833551053816,
+        speed: 324,
+        gear: 8
+},
+        {
+        longitude: 9.289412498474121,
+        latitude: 45.6249931957286,
+        speed: 339,
+        gear: 8
+},
+        {
+        longitude: 9.288468360900879,
+        latitude: 45.624422947440564,
+        speed: 324,
+        gear: 8
+},
+        {
+        longitude: 9.287395477294922,
+        latitude: 45.623942733856815,
+        speed: 284,
+        gear: 8
+},
+        {
+        longitude: 9.286837577819822,
+        latitude: 45.62334246109251,
+        speed: 257,
+        gear: 7
+},
+        {
+        longitude: 9.286408424377441,
+        latitude: 45.623042322300115,
+        speed: 230,
+        gear: 6
+},
+        {
+        longitude: 9.286236763000488,
+        latitude: 45.62289225230136,
+        speed: 220,
+        gear: 5
+},
+        {
+        longitude: 9.286065101623535,
+        latitude: 45.6227421819009,
+        speed: 210,
+        gear: 5
+},
+        {
+        longitude: 9.28589344024658,
+        latitude: 45.6225020684246,
+        speed: 199,
+        gear: 5
+},
+        {
+        longitude: 9.285936355590819,
+        latitude: 45.622321982642525,
+        speed: 197,
+        gear: 5
+},
+        {
+        longitude: 9.28589344024658,
+        latitude: 45.62205185288477,
+        speed: 199,
+        gear: 5
+},
+        {
+        longitude: 9.285764694213867,
+        latitude: 45.62181173645191,
+        speed: 203,
+        gear: 5
+},
+        {
+        longitude: 9.285635948181152,
+        latitude: 45.62154160423572,
+        speed: 209,
+        gear: 5
+},
+        {
+        longitude: 9.285335540771484,
+        latitude: 45.621361515368505,
+        speed: 219,
+        gear: 5
+},
+        {
+        longitude: 9.285292625427246,
+        latitude: 45.621181425922835,
+        speed: 237,
+        gear: 6
+},
+        {
+        longitude: 9.284949302673338,
+        latitude: 45.62052109300635,
+        speed: 257,
+        gear: 6
+},
+        {
+        longitude: 9.284648895263672,
+        latitude: 45.61929045181886,
+        speed: 279,
+        gear: 7
+},
+        {
+        longitude: 9.284477233886719,
+        latitude: 45.6180297669855,
+        speed: 300,
+        gear: 7
+},
+        {
+        longitude: 9.284348487854004,
+        latitude: 45.617219311767826,
+        speed: 333,
+        gear: 8
+},
+        {
+        longitude: 9.284262657165527,
+        latitude: 45.61619872186784,
+        speed: 333,
+        gear: 8
+},
+        {
+        longitude: 9.28391933441162,
+        latitude: 45.61460776527223,
+        speed: 333,
+        gear: 8
+},
+        {
+        longitude: 9.283790588378906,
+        latitude: 45.613136840717544,
+        speed: 313,
+        gear: 8
+},
+        {
+        longitude: 9.283747673034668, 
+        latitude: 45.612746588951964,
+        speed: 273,
+        gear: 7
+},
+        {
+        longitude: 9.283661842346191,
+        latitude: 45.61247641306195,
+        speed: 210,
+        gear: 6
+},
+        {
+        longitude: 9.283576011657715,
+        latitude: 45.61229629507887,
+        speed: 219,
+        gear: 6
+},
+        {
+        longitude: 9.283404350280762,
+        latitude: 45.612116176517304,
+        speed: 227,
+        gear: 6
+},
+        {
+        longitude: 9.283146858215332,
+        latitude: 45.61196607727412,
+        speed: 236,
+        gear: 6
+},
+        {
+        longitude: 9.282846450805664,
+        latitude: 45.61181597762924,
+        speed: 251,
+        gear: 6
+},
+        {
+        longitude: 9.282116889953613,
+        latitude: 45.61184599759034,
+        speed: 263,
+        gear: 6
+},
+        {
+        longitude: 9.281516075134277,
+        latitude: 45.612116176517304,
+        speed: 270,
+        gear: 7
+},
+        {
+        longitude: 9.28095817565918,
+        latitude: 45.61247641306195,
+        speed: 281,
+        gear: 7
+},
+        {
+        longitude: 9.280357360839844,
+        latitude: 45.61319687920969,
+        speed: 290,
+        gear: 7
+},
+        {
+        longitude: 9.280314445495605,
+        latitude: 45.61373722274664,
+        speed: 296,
+        gear: 7
+},
+        {
+        longitude: 9.280571937561035,
+        latitude: 45.61487793089452,
+        speed: 300,
+        gear: 8
+},
+        {
+        longitude: 9.280915260314941,
+        latitude: 45.616258757082036,
+        speed: 307,
+        gear: 8
+},
+        {
+        longitude: 9.28095817565918,
+        latitude: 45.617189294682966,
+        speed: 313,
+        gear: 8
+},
+        {
+        longitude: 9.281044006347654,
+        latitude: 45.61757951553293,
+        speed: 318,
+        gear: 8
+}
+
+]
